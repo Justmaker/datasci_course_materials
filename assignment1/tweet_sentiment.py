7251593612ba1270c52e3faf71cb19e77a7264e0
Alexis Vanacker <alexvanacker@gmail.com>
2014-07-03 22:58:44 +0200

    feat(tweet): start extracting sentiment from tweets - basic level

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,56 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def test_10_tweets(tweet_file, sent_dict):
+    """Test on 10 first lines from tweet file
+
+    """
+    count = 0
+
+    for line in tweet_file.readlines():
+        tweet_infos = json.loads(line)
+        try:
+
+            if 'text' in tweet_infos.keys():
+                text = tweet_infos['text']
+                if text:
+                    analyse_text(text, sent_dict)
+                else:
+                    print 'error'
+                count += 1
+                if count > 50:
+                    break
+        except Exception, e:
+            print 'Error on tweet:'
+            print tweet_infos
+            print str(e)
+            raise
+
+
+def extract_sent_dict(sent_file_path):
+    afinnfile = open(sent_file_path)
+    scores = {}
+    for line in afinnfile:
+        term, score = line.split("\t")
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    return scores
+
+
+def analyse_text(text, sent_dict):
+    words = text.split()
+    score = 0
+    for word in words:
+        uni_word = word.encode('utf-8')
+        score += sent_dict[word] if uni_word in sent_dict.keys() else 0
+    print score
+
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sent_dict = extract_sent_dict('AFINN-111.txt')
+    tweet_file = open('output.txt')
+    test_10_tweets(tweet_file, sent_dict)
 
 if __name__ == '__main__':
     main()
